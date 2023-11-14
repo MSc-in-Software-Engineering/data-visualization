fd8409a87a3d3648f69a9534f3bbf763decb5b26
Wahid Tobias <waraz19@student.sdu.dk>
2023-11-14 19:25:45 +0100

    added introduction text

--- Shiny/pages/page_2.R
+++ Shiny/pages/page_2.R
@@ -7,6 +7,8 @@ page_2 <- function(input, output) {
         fluidRow(
             column(8, plotOutput("gdp_growth_rate_map"))
         ),
+        h2("Subsection 2"),
+        p("N/A"),
         fluidRow(
             column(8, plotOutput("government_education_expenditure_map"))
         )
