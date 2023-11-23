96ff81a07c125f9005956b2e4c73c21146dbd3d0
Sathveekan <sathveekan23@gmail.com>
2023-11-23 12:49:18 +0100

    Added regression lines for scatterplot

--- Shiny/plots/scatterplots/birth_death_rate_scatterplot.R
+++ Shiny/plots/scatterplots/birth_death_rate_scatterplot.R
@@ -30,7 +30,7 @@ birth_rate_data <- birth_rate_data %>%
 ################################################################################
 country_1 <- c("India")  
 
-# Filtering out non existing values
+# Filtering out non-existing values
 filtered_death_rate_data <- death_rate_data %>%
   filter(`Country Name` %in% country_1 & !is.na(Death_rate))
 
@@ -38,40 +38,58 @@ filtered_death_rate_data <- death_rate_data %>%
 filtered_birth_rate_data <- birth_rate_data %>%
   filter(`Country Name` %in% country_1 & !is.na(Birth_rate))
 
+# Remove rows with non-finite values
+filtered_death_rate_data <- na.omit(filtered_death_rate_data)
+filtered_birth_rate_data <- na.omit(filtered_birth_rate_data)
+
 scatterPlot_1 <- ggplot() +
-  geom_point(data = filtered_death_rate_data, aes(x = factor(Year), y = as.numeric(Death_rate), color = "Death Rate", text = paste("Year: ", Year, "<br>Death Rate: ", Death_rate)), size = 3) +
-  geom_point(data = filtered_birth_rate_data, aes(x = factor(Year), y = as.numeric(Birth_rate), color = "Birth Rate", text = paste("Year: ", Year, "<br>Birth Rate: ", Birth_rate)), size = 3) +
+  geom_point(data = filtered_death_rate_data, aes(x = Year, y = as.numeric(Death_rate), color = "Death Rate", text = paste("Year: ", Year, "<br>Death Rate: ", Death_rate)), size = 3) +
+  geom_point(data = filtered_birth_rate_data, aes(x = Year, y = as.numeric(Birth_rate), color = "Birth Rate", text = paste("Year: ", Year, "<br>Birth Rate: ", Birth_rate)), size = 3) +
   scale_color_manual(values = c("Death Rate" = "red", "Birth Rate" = "green")) +
   labs(x = "Year", y = "Rate (per 1,000 people)", title = "Death and Birth Rate of India Over Years") +
   theme(legend.position = "bottom") +
-  labs(color = " ")
+  labs(color = " ") +
+  # Add regression lines using geom_smooth
+  geom_smooth(data = filtered_death_rate_data, aes(x = Year, y = as.numeric(Death_rate), color = "Death Rate"), method = "lm", se = FALSE, na.rm = TRUE) +
+  geom_smooth(data = filtered_birth_rate_data, aes(x = Year, y = as.numeric(Birth_rate), color = "Birth Rate"), method = "lm", se = FALSE, na.rm = TRUE) +
+  # Adjust x-axis breaks to display whole numbers
+  scale_x_continuous(breaks = seq(min(filtered_death_rate_data$Year), max(filtered_death_rate_data$Year), by = 1))
 
 # Modify tooltip content
-birth_death_rate_india_scatterplot <- function() {
-  ggplotly(scatterPlot_1, tooltip = "text")
-}
+ggplotly(scatterPlot_1, tooltip = "text")
+
 
 ################################################################################
 
 # Modify tooltip content
 birth_death_rate_scatterplot <- function(selectedCountry) {
   country_2 <- c(selectedCountry)  
-
-  # Filtering out non existing values
+  
+  
+  # Filtering out non-existing values
   filtered_death_rate_data <- death_rate_data %>%
     filter(`Country Name` %in% country_2 & !is.na(Death_rate))
-
+  
   # Filter for the selected countries and non-missing values for birth rate
   filtered_birth_rate_data <- birth_rate_data %>%
     filter(`Country Name` %in% country_2 & !is.na(Birth_rate))
-
+  
+  # Remove rows with non-finite values
+  filtered_death_rate_data <- na.omit(filtered_death_rate_data)
+  filtered_birth_rate_data <- na.omit(filtered_birth_rate_data)
+  
   scatterPlot_2 <- ggplot() +
-    geom_point(data = filtered_death_rate_data, aes(x = factor(Year), y = as.numeric(Death_rate), color = "Death Rate", text = paste("Year: ", Year, "<br>Death Rate: ", Death_rate)), size = 3) +
-    geom_point(data = filtered_birth_rate_data, aes(x = factor(Year), y = as.numeric(Birth_rate), color = "Birth Rate", text = paste("Year: ", Year, "<br>Birth Rate: ", Birth_rate)), size = 3) +
+    geom_point(data = filtered_death_rate_data, aes(x = Year, y = as.numeric(Death_rate), color = "Death Rate", text = paste("Year: ", Year, "<br>Death Rate: ", Death_rate)), size = 3) +
+    geom_point(data = filtered_birth_rate_data, aes(x = Year, y = as.numeric(Birth_rate), color = "Birth Rate", text = paste("Year: ", Year, "<br>Birth Rate: ", Birth_rate)), size = 3) +
     scale_color_manual(values = c("Death Rate" = "red", "Birth Rate" = "green")) +
     labs(x = "Year", y = "Rate (per 1,000 people)", title = paste("Death and Birth Rate of", selectedCountry ,"Over Years")) +
     theme(legend.position = "bottom") +
-    labs(color = " ") 
+    labs(color = " ") +
+    # Add regression lines using geom_smooth
+    geom_smooth(data = filtered_death_rate_data, aes(x = Year, y = as.numeric(Death_rate), color = "Death Rate"), method = "lm", se = FALSE, na.rm = TRUE) +
+    geom_smooth(data = filtered_birth_rate_data, aes(x = Year, y = as.numeric(Birth_rate), color = "Birth Rate"), method = "lm", se = FALSE, na.rm = TRUE) +
+    # Adjust x-axis breaks to display whole numbers
+    scale_x_continuous(breaks = seq(min(filtered_death_rate_data$Year), max(filtered_death_rate_data$Year), by = 1))
 
    ggplotly(scatterPlot_2, tooltip = "text")
 }
